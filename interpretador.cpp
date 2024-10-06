ade893fb567f79ff84e94a8790a4743dfa1d3cca
Daniel dos Santos <danieldossantxs@gmail.com>
2024-10-06 15:50:07 -0300

    Funcionando

--- interpretador.cpp
+++ interpretador.cpp
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <conio2.h>
 
+// Estruturas
 struct pLinha {
     char token[50];
     struct pLinha *prox;
@@ -13,9 +15,42 @@ struct pPrograma {
     struct pPrograma *ant, *prox;
 }; typedef struct pPrograma Prog;
 
+struct pilhaLilha {
+    Lin *info;
+    struct pilhaLilha *prox;
+}; typedef struct pilhaLilha PilhaL;
 
+// Funções - Pilha
+void initPL(PilhaL **PL) {
+    *PL = NULL;
+}
+
+void pushL(PilhaL **PL, Lin *x) {
+    PilhaL *nova = (PilhaL*) malloc(sizeof(PilhaL));
+    nova->info = x;
+    nova->prox = *PL;
+    *PL = nova;
+}
+
+char isEmptyPL(PilhaL *PL) {
+    return PL == NULL;
+}
+
+void popL(PilhaL **PL, Lin **x) {
+    PilhaL *aux;
+    if (!isEmptyPL(*PL)) {
+        aux = *PL;
+        *x = (*PL)->info;
+        *PL = (*PL)->prox;
+        free(aux);
+    } else {
+        *x = NULL;
+    }
+}
+
+// Funções
 Lin* novaLinha(char* valor) {
-    Lin* nova = (Lin*)malloc(sizeof(Lin));
+    Lin* nova = (Lin*) malloc(sizeof(Lin));
     strcpy(nova->token, valor);
     nova->prox = NULL;
     return nova;
@@ -35,7 +70,7 @@ void adicionarToken(Lin** linha, char* valor) {
 }
 
 Prog* novoPrograma() {
-    Prog* novo = (Prog*)malloc(sizeof(Prog));
+    Prog* novo = (Prog*) malloc(sizeof(Prog));
     novo->linha = NULL;
     novo->ant = NULL;
     novo->prox = NULL;
@@ -59,20 +94,7 @@ void extrairTokens(char* linha, Lin** linhaTokens) {
     int inicio = 0, fim = 0, tamanho = strlen(linha);
     char token[50];
     int extrairAntesDoParenteses = 0; 
-
-    while (linha[fim] != ' ' && linha[fim] != '\t' && linha[fim] != '\n' && linha[fim] != '(' && linha[fim] != '\0') {
-        fim++;
-    }
-
-    strncpy(token, linha, fim - inicio);
-    token[fim - inicio] = '\0';
-
-    if (strcmp(token, "def") == 0 || strcmp(token, "print") == 0) {
-        extrairAntesDoParenteses = 1;
-    }
-
-    inicio = 0;
-    fim = 0;
+    int dentroParenteses = 0;
 
     while (fim <= tamanho) {
         if (linha[fim] == ' ' || linha[fim] == '\t' || linha[fim] == '\n' || linha[fim] == '\0') {
@@ -83,15 +105,14 @@ void extrairTokens(char* linha, Lin** linhaTokens) {
             }
             inicio = fim + 1;
         } else if (linha[fim] == '(') {
-            if (extrairAntesDoParenteses && fim > inicio) {
-                strncpy(token, &linha[inicio], fim - inicio);
-                token[fim - inicio] = '\0';
-                adicionarToken(linhaTokens, token);
-            }
+            strncpy(token, &linha[inicio], fim - inicio);
+            token[fim - inicio] = '\0';
+            adicionarToken(linhaTokens, token);
             token[0] = '(';
             token[1] = '\0';
             adicionarToken(linhaTokens, token);
             inicio = fim + 1;
+            dentroParenteses = 1;
         } else if (linha[fim] == ')') {
             if (fim > inicio) {
                 strncpy(token, &linha[inicio], fim - inicio);
@@ -99,6 +120,15 @@ void extrairTokens(char* linha, Lin** linhaTokens) {
                 adicionarToken(linhaTokens, token);
             }
             token[0] = ')';
+            token[1] = '\0';
+            adicionarToken(linhaTokens, token);
+            inicio = fim + 1;
+            dentroParenteses = 0;
+        } else if (linha[fim] == ',') {
+            strncpy(token, &linha[inicio], fim - inicio);
+            token[fim - inicio] = '\0';
+            adicionarToken(linhaTokens, token);
+            token[0] = ',';
             token[1] = '\0';
             adicionarToken(linhaTokens, token);
             inicio = fim + 1;
@@ -131,10 +161,18 @@ void lerArquivoPython(Prog** p, char* caminhoArquivo) {
     *p = programa;
 }
 
-void imprimirPrograma(Prog* programa) {
+void imprimirPrograma(Prog* programa, Prog* linhaExec) {
+    system("cls");
     Prog* progAtual = programa;
     while (progAtual != NULL) {
         Lin* linhaAtual = progAtual->linha;
+        if (progAtual == linhaExec) {
+            textbackground(CYAN);
+            textcolor(BLACK);
+        } else {
+            textbackground(BLACK);
+            textcolor(WHITE);
+        }
         while (linhaAtual != NULL) {
             printf("%s ", linhaAtual->token);
             linhaAtual = linhaAtual->prox;
@@ -144,16 +182,75 @@ void imprimirPrograma(Prog* programa) {
     }
 }
 
+Prog* encontrarPrimeiraLinhaExecutavel(Prog* programa) {
+    Prog* progAtual = programa;
+    int fimDef = 1;
 
-int main(void) {
-    Prog *p = NULL;
-    char caminhoArquivo[50];
-    strcpy(caminhoArquivo, "C://Trabalho//main.py");
-    
-    lerArquivoPython(&p, caminhoArquivo);
-    
-    imprimirPrograma(p);
-    
-    return 0;
+    while (progAtual != NULL) {
+        Lin* linhaAtual = progAtual->linha;
+
+        if (linhaAtual != NULL) {
+            if (strcmp(linhaAtual->token, "def") == 0)
+                fimDef = 0;
+            else if (strcmp(linhaAtual->token, "fim-def") == 0)
+                fimDef = 1;
+            else if (fimDef == 1)
+                return progAtual;
+        }
+
+        progAtual = progAtual->prox;
+    }
+
+    return NULL;
 }
 
+Prog* encontrarFuncao(Prog* programa, char* nomeFuncao) {
+    Prog* progAtual = programa;
+    while (progAtual != NULL) {
+        Lin* linhaAtual = progAtual->linha;
+        if (linhaAtual != NULL && strcmp(linhaAtual->token, "def") == 0) {
+            linhaAtual = linhaAtual->prox; 
+            if (linhaAtual != NULL && strcmp(linhaAtual->token, nomeFuncao) == 0) {
+                return progAtual;
+            }
+        }
+        progAtual = progAtual->prox;
+    }
+    return NULL; 
+}
+
+int main(void) {
+    Prog *p = NULL, *linhaExec;
+    char tecla;
+    char caminhoArquivo[50];
+    
+    while (1) {
+        if (kbhit()) {
+            tecla = getch();
+            
+            if (tecla == 65) { // F7 - Abrir arquivo
+                //printf("Digite o caminho do arquivo: ");
+                //scanf("%s", caminhoArquivo);
+                strcpy(caminhoArquivo, "C://Trabalho//main.py");
+                lerArquivoPython(&p, caminhoArquivo);
+                linhaExec = encontrarPrimeiraLinhaExecutavel(p);
+                imprimirPrograma(p, linhaExec);
+            }
+            
+            if (tecla == 66) { // F8 - Executar passo a passo
+                while (linhaExec != NULL && tecla != 27) { // ESC
+                    if (kbhit()) {
+                        tecla = getch();
+                        if (tecla == 13) { // ENTER
+                            linhaExec = linhaExec->prox;
+                            imprimirPrograma(p, linhaExec);
+                        }
+                    }
+                }
+            }
+            
+            if (tecla == 27) // ESC
+                return 0;
+        }
+    }
+}
--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-def calcula(x, y):
-    return x + y
+def expressao(a, b):
+    return a + b    
 fim-def
 
-a = 5
-b = 10
-c = 0
-c = calcula(a, b)
-print("%d %d %d" % (a, b, c))
-a = calcula(7, a+b+c)
-print("%s %d %d" % ("7", a+b+c, a))
-c = calcula(a*b, a//b)
-print("%d %d %d" % (a*b, a//b, c))
-a = calcula(b, b+b)
-msg = 'resultado ='
-print("%s %d %d %d" % (msg, a, b, c))
+def contagem_while():
+    contador = 0
+    print("Contagem usando while:")
+    while contador < 5:
+        print(contador)
+        contador += 1
+fim-def
+
+def percorre_lista():
+    lista = [10, 20, 30, 40]
+    print("Percorrendo a lista usando for:")
+    for item in lista:
+        print(item)
+fim-def
+
+print("Bem-vindo ao programa de exemplo em Python!")
+
+resultado = expressao(4, 7)
+if resultado > 10:
+    print("O resultado e maior que 10.")
+elif resultado == 10:
+    print("O resultado e exatamente 10.")
+else:
+    print("O resultado e menor que 10.")
+
+lista = [5, 2, 9, 1]
+print("Lista original:", lista)
+    
+lista . append(7)
+print("Apos adicionar 7:", lista)
+    
+lista . remove(2)
+print("Apos remover 2:", lista)
+    
+lista . sort()
+print("Lista ordenada:", lista)
+    
+lista . pop()
+print("Apos remover o ultimo elemento:", lista)
+
+contagem_while()
+percorre_lista()
